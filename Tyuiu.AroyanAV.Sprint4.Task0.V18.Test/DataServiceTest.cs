877ee1643398bddca1be11af9b2eda5e1c0a368d
Armine Aroyan <aroanarmine068@gmail.com>
2025-11-24 17:21:29 +0500

    Add project files.

---
+++ Tyuiu.AroyanAV.Sprint4.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AroyanAV.Sprint4.Task0.V18.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint4.Task0.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] a = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+            int res = ds.GetSumOddArrEl(a);
+            int wait = 41;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
