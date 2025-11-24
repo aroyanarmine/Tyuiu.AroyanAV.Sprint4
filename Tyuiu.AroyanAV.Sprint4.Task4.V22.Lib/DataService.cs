d9776bcd415ed5ff4e493aca029b749aca91fd1e
Armine Aroyan <aroanarmine068@gmail.com>
2025-11-24 20:24:20 +0500

    Task4

---
+++ Tyuiu.AroyanAV.Sprint4.Task4.V22.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.AroyanAV.Sprint4.Task4.V22.Lib
+{
+    public class DataService : ISprint4Task4V22
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int str = matrix.GetUpperBound(0) + 1;
+            int stolb = matrix.Length / str;
+
+            for (int i = 0; i < str; i++)
+            {
+                for (int j = 0; j < stolb; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            return matrix;
+
+        }
+    }
+}
---
+++ Tyuiu.AroyanAV.Sprint4.Task4.V22.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.AroyanAV.Sprint4.Task4.V22.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint4.Task4.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[5, 5] {{4, 3, 2, 4, 5},
+                                           {5, 4, 5, 6, 6},
+                                           {6, 2, 3, 6, 5},
+                                           {6, 2, 3, 6, 4},
+                                           {6, 2, 6, 6, 2} };
+
+            int[,] res = ds.Calculate(matrix);
+
+            int[,] wait = new int[5, 5] {{4, 0, 2, 4, 0},
+                                           {0, 4, 0, 6, 6},
+                                           {6, 2, 0, 6, 0},
+                                           {6, 2, 0, 6, 4},
+                                           {6, 2, 6, 6, 2} };
+
+            CollectionAssert.AreEqual(wait, res);
+
+
+        }
+    }
+}
