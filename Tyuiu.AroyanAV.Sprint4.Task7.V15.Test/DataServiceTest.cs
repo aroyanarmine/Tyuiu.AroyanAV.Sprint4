bb37fa0431efe51323ee6dc3ac6160f046a3cdac
Armine Aroyan <aroanarmine068@gmail.com>
2025-11-28 17:00:06 +0500

    Task7

---
+++ Tyuiu.AroyanAV.Sprint4.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AroyanAV.Sprint4.Task7.V15.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint4.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int rows = 4;
+            int colums = 2;
+            int[,] mtrx = new int[rows, colums];
+            string str = "10293847";
+            int res = ds.Calculate(rows, colums, str);
+            int wait = 4;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.AroyanAV.Sprint4.Task7.V15/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.AroyanAV.Sprint4.Task7.V15.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Ароян А. В. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнила: Ароян Армине Вачагановна | ИСТНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дана строка из одноразрядных цифр \"10293847\". Преобразуйте ее в       *");
+        Console.WriteLine("* матрицу 4 на 2 и подсчитайте количество нечетных чисел в матрице        *");
+        Console.WriteLine("*                                                                         *");
+
+        int rows = 4;
+        int colums = 2;
+        int[,] mtrx = new int[rows, colums];
+        string str = "10293847";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int index = 0;
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < colums; j++)
+            {
+                Console.Write($"{str[index]} \t");
+                index++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        
+        int res = ds.Calculate(rows, colums, str);
+       
+        Console.WriteLine("Количество нечетных элементов = " + res);
+        Console.ReadKey();
+    }
+}
